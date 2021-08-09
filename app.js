834ae8618c8160e058c6b06f0bfa8ce9732b0c97
hariNEzuMI928 <dai.suzuki.829@gmail.com>
2021-08-10 08:58:31 +0900

    handlersを追加

--- app.js
+++ app.js
@@ -2,24 +2,22 @@ require("dotenv").config();
 fetch = require("node-fetch");
 
 const { app } = require("./services/slackService");
+const handlers = require("./handlers");
 const { TIME_CLOCK_TYPE} = require("./services/commonService");
-const handleStartAttendanceModal = require("./handlers/handleStartAttendanceModal");
-const handleStartAttendanceShortcut = require("./handlers/handleStartAttendanceShortcut");
-const handlePostTimeClockAction = require("./handlers/handlePostTimeClockAction");
 
 // 勤怠打刻開始モーダル
-app.shortcut("start_attendance_shortcut", handleStartAttendanceShortcut);
+app.shortcut("start_attendance_shortcut", handlers.handleStartAttendanceShortcut);
 
 // 勤怠打刻開始モーダルでのセレクトボックス選択イベント
 app.action("select_location_action", async ({ ack }) => await ack());
 
 // 勤怠打刻開始モーダルでのデータ送信イベント
-app.view("start_attendance_modal", handleStartAttendanceModal);
+app.view("start_attendance_modal", handlers.handleStartAttendanceModal);
 
 // Buttonアクション
-app.action(TIME_CLOCK_TYPE.break_begin.value, handlePostTimeClockAction);
-app.action(TIME_CLOCK_TYPE.break_end.value, handlePostTimeClockAction);
-app.action(TIME_CLOCK_TYPE.clock_out.value, handlePostTimeClockAction);
+app.action(TIME_CLOCK_TYPE.break_begin.value, handlers.handlePostTimeClockAction);
+app.action(TIME_CLOCK_TYPE.break_end.value, handlers.handlePostTimeClockAction);
+app.action(TIME_CLOCK_TYPE.clock_out.value, handlers.handlePostTimeClockAction);
 
 (async () => {
   await app.start();
---
+++ handlers/index.js
@@ -0,0 +1,9 @@
+const handleStartAttendanceModal = require("./handleStartAttendanceModal");
+const handleStartAttendanceShortcut = require("./handleStartAttendanceShortcut");
+const handlePostTimeClockAction = require("./handlePostTimeClockAction");
+
+module.exports = {
+    handleStartAttendanceModal: handleStartAttendanceModal,
+    handleStartAttendanceShortcut: handleStartAttendanceShortcut,
+    handlePostTimeClockAction: handlePostTimeClockAction,
+};
