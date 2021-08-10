5da6fb02e1fb141c8223ed89c7cb1660997a27c5
hariNEzuMI928 <dai.suzuki.829@gmail.com>
2021-08-10 09:58:20 +0900

    src配下に移動

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,14 @@
 require("dotenv").config();
-fetch = require("node-fetch");
 
 const { app } = require("./services/slackService");
 const handlers = require("./handlers");
-const { TIME_CLOCK_TYPE} = require("./services/commonService");
+const { TIME_CLOCK_TYPE } = require("./services/commonService");
 
 // 勤怠打刻開始モーダル
 app.shortcut("start_attendance_shortcut", handlers.handleStartAttendanceShortcut);
 
 // 勤怠打刻開始モーダルでのセレクトボックス選択イベント
-app.action("select_location_action", async ({ ack }) => await ack());
+app.action("select_location_action", async ({ ack }: { ack: any }) => await ack());
 
 // 勤怠打刻開始モーダルでのデータ送信イベント
 app.view("start_attendance_modal", handlers.handleStartAttendanceModal);
